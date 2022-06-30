60baf5a563968196822aa1d62facff3239722019
Temiwunmi <temiwunmibowoto@gmail.com>
2022-06-30 04:11:39 -0700

    add

---
+++ 0x00-hello_world/main.c
@@ -0,0 +1,9 @@
+nclude <stdio.h>                                                                                                          
+
+int main ()                                                                                                                 
+	                                                                                                                           
+{                                                                                                                           
+
+	    printf ("Hello World!\n");                                                                                                        
+	    return 0; 
+}    
