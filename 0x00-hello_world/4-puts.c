37049ed3cb5f11c0806d0e219acfe2edea5653e2
Temiwunmi <temiwunmibowoto@gmail.com>
2022-06-30 06:56:26 -0700

    hot

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,6 +2,14 @@
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	/**
+	 * char = programming is like a multilingual puzzle
+	 */
+	char str[] = "\"Programming is like building a multilingual puzzle\n";
+       /**
+	* puts = input the str
+	*/
+	puts(str);
 	return (0);
 }
+
