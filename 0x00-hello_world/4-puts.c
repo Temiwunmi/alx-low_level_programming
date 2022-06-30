ea50a9d73d1ce5079b81a05e507ae15a4a57d2d7
Temiwunmi <temiwunmibowoto@gmail.com>
2022-06-30 05:37:15 -0700

    put4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,7 @@
 #include <stdio.h>
 
-#include <string.h>
-
-
-
-int main() {
-
-	 
-
-	    char str1[] = "Programming is like building a multilingual puzzle\n";
-	          puts(str1);
-	               return 0;
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
 }
