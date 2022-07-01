fe9b58a711fa05cfa74c98b312e4d580a2064b7c
Temiwunmi <temiwunmibowoto@gmail.com>
2022-06-30 18:47:41 -0700

    hello

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - print with proper grammer
+ * Return: 0 if exited properly, non-zero otherwise
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
