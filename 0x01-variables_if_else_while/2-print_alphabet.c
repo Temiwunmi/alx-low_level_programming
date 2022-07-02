2b2e4dac5f2539edf0f23b1b0f05486cd0971868
Temiwunmi <temiwunmibowoto@gmail.com>
2022-07-01 17:57:19 -0700

    t

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *main - print numbers using even
+ *Return: 0 if exited properly
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 97; ch <= 122; ch++)
+	{
+		putchar (ch);
+	}
+	putchar (10);
+	return (0);
+}
