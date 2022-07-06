d32a3edb9da32259b6f7af8e67b7c5c4227e9817
Temiwunmi <temiwunmibowoto@gmail.com>
2022-07-06 13:43:00 -0700

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,26 +1,25 @@
 #include "main.h"
 
 /**
- * print_sign - print signs
- * @n: integer
- * Return: 1, 0 and -1 if greater less and equal
+ * print_sign - printsn
+ * @n: character
+ * Return: 1, 0 and -1
  */
 int print_sign(int n)
 {
 	if (n > 0)
-		{
-			_putchar ('+');
-			return (1);
-		}
+	{
+		_putchar('+');
+		return (1);
+	}
 	else if (n == 0)
-		{	
-			_putchar ('0');
-			return (0);
-		}
+	{
+		_putchar(0);
+		return (0);
+	}
 	else
-		(n < 0)
-		{
-			_putchar ('-');
-			return (-1);
-		}
+	{
+		_putchar('-');
+		return (-1);
+	}
 }
