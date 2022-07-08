a9c546610b4f8a4a93f11f47a351a4a2e039f94d
Temiwunmi <temiwunmibowoto@gmail.com>
2022-07-07 17:30:13 -0700

    printdiagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_diagonal - print diagonal shape
+ * @n: integer parameter
+ */
+void print_diagonal(int n)
+{
+	int b, c;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (b = 0; b < n; b++)
+		{
+			for (c = 0; c < b; b++)
+				_putchar(' ');
+			_putchar(92);
+			_putchar('\n');
+		}
+	}
+}
