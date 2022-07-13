85f778d903770383fc81e066a04e501e3a191c29
Temiwunmi <temiwunmibowoto@gmail.com>
2022-07-13 15:27:14 -0700

    string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the lenght of a string
+ * @s: string to be passed
+ * Return: lenght of a  string
+ */
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
