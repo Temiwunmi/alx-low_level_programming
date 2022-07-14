88e1038d51c5863d74edeea7a00cea191e1da5a0
Temiwunmi <temiwunmibowoto@gmail.com>
2022-07-13 18:24:35 -0700

    puts

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 
 /**
  * puts2 - print character of a string
@@ -9,7 +10,7 @@ void puts2(char *str)
 	int len, i;
 
 	len = strlen(str);
-	for (i = 0; i < len; i += 2)
+	for (i = 0; i < len; i++)
 		_putchar(str[i]);
 	_putchar('\n');
 }
