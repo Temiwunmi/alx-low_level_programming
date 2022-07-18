c08462fb58c18785e6e219b41406714c9eb2b6ce
Temiwunmi <temiwunmibowoto@gmail.com>
2022-07-18 12:14:42 -0700

    memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _memset - memory set function
+ * @s: string
+ * @b: a charcter
+ * @n: an integer
+ * Return: string
+ */
+char *_memset(char *s, char b, unsigned int n);
+{
+	unsigned int x;
+
+	for (x = 0; x < n; x++
+		s[x] = b;
+		return (s);
+}
