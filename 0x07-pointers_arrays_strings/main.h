89fed60af5dc7b47910f70fd26ea0271b6a2d6f4
Temiwunmi <temiwunmibowoto@gmail.com>
2022-07-19 14:16:05 -0700

    strbrk

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -6,6 +6,7 @@ int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
 
