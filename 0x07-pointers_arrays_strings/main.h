9d17c1fc96e86e7eb5b270a3be2b098fb916b168
Temiwunmi <temiwunmibowoto@gmail.com>
2022-07-19 13:15:59 -0700

    strchr

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -4,6 +4,7 @@
 
 int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
+char *_strchr(char *s, char c);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
 
