5b0a3d09424e9c67ffbaa9d8011e2ab14bae2763
Temiwunmi <temiwunmibowoto@gmail.com>
2022-07-27 12:32:59 -0700

    arg

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints all args
+ * @argc: args count
+ * @argv: args vector
+ * Return: return 0
+ */
+int main(int argc, char *argv[])
+{
+	int n;
+
+	for (n - 0; n < argc; n++)
+	{
+		printf("%s\n", argv[n]);
+	}
+}
