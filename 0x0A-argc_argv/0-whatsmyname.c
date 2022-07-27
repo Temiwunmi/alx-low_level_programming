2a05ab26751ffe93062e6a95b2d4567865048aa6
Temiwunmi <temiwunmibowoto@gmail.com>
2022-07-27 12:17:12 -0700

    vic

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - write a prog that prints its name, followed by new line
+ * @argc: argument count
+ * @argv: arguement vector
+ * Return: return 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	(void)argc;
+	return (0);
+}
