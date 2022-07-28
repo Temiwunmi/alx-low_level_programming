ac84b14d47221374a705830426b51237f4c960bd
Temiwunmi <temiwunmibowoto@gmail.com>
2022-07-27 17:11:44 -0700

    100

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,51 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * argstostr - arguement of the prog
+ * @ac: arg count
+ * @av: points to vector
+ * Return: NUll
+ */
+char *argstostr(int ac, char **av)
+{
+	int i, j, k, size;
+	char *arg;
+
+	size = 0;
+	k = 0;
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	i = 0;
+	while (i < ac)
+	{
+		j = 0;
+		while (av[i][j])
+		{
+			size++;
+			j++;
+		}
+		size++;
+		i++;
+	}
+	arg = malloc((sizeof(char) * size) + 1);
+	if (arg == NULL)
+		return (NULL);
+	i = 0;
+	while (i < ac)
+	{
+		j = 0;
+		while (av[i][j])
+		{
+			arg[k] = av[i][j];
+			j++;
+			k++;
+		}
+		arg[k] = '\n';
+		k++;
+		j++;
+	}
+	arg[k] = '\0';
+	return (arg);
+}
+
