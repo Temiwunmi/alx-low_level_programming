1c95d6503067ccff2608e7a4acca33afc6a34989
Temiwunmi <temiwunmibowoto@gmail.com>
2022-08-11 04:43:52 -0700

    printlist

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * print_list- singly linked list
+ * @h: element on linked list
+ * Return: number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t x = 0;
+
+	while (h)
+	{
+		if (h->str == NULL)
+			printf("[%d] %s\n", 0, "(nil)");
+		else
+			print("[%d] %s\n", h->len, h->str);
+
+		x++;
+		h = h->next;
+	}
+	return (x);
+}
